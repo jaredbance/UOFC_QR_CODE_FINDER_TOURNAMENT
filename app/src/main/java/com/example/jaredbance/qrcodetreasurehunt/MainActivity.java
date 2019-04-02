a405d7edad6e5a9ac7ee8cdca2b4ab0cda38f07e
Jared <jaredbance@gmail.com>
2019-04-01 18:39:22 -0600

    first commit

---
+++ app/src/main/java/com/example/jaredbance/qrcodetreasurehunt/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.jaredbance.qrcodetreasurehunt;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginscreen);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //
+                // DO LOGIN VERIFICATION
+                //
+                startActivity(new Intent(MainActivity.this, HomeScreen.class));
+
+            }
+        });
+    }
+
+
+
+
+}

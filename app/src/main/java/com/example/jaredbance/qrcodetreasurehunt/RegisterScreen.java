ef86a9e326ca6f7b15b27a8172e465e70630f156
shawnamckay <shawnammckay@gmail.com>
2019-04-03 11:59:35 -0600

    Added some basic navigation logic

--- app/src/main/java/com/example/jaredbance/qrcodetreasurehunt/RegisterScreen.java
+++ app/src/main/java/com/example/jaredbance/qrcodetreasurehunt/RegisterScreen.java
@@ -10,9 +10,28 @@ import android.widget.Button;
 public class RegisterScreen extends AppCompatActivity {
 
 
+    private Button submitButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerscreen);
+
+
+
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //
+                //Need to check that username is unique 
+                //
+
+                startActivity(new Intent(RegisterScreen.this, HomeScreen.class));
+
+            }
+        });
     }
 }
